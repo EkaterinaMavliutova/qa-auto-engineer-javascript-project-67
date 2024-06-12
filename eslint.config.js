ff86353614884f26315d9c576bfa6d654dccbbc7
ekaterinamavliutova <akumaaya@gmail.com>
2024-06-12 18:31:18 +0200

    update config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
-import globals from "globals";
-
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+/* eslint no-underscore-dangle: ["error", { "allow": ["__filename", "__dirname"] }] */
+import globals from 'globals';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
   {
@@ -17,20 +20,34 @@ export default [
       globals: {
         ...globals.es2021,
         ...globals.node,
+        ...globals.jest,
       },
-      ecmaVersion: latest,
-      sourceType: module,
-    }
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
   },
   {
-    rules: {
-      "no-console": "off",
-      "import/extensions": [
-        "error",
-        "ignorePackages",
-        { "js": "always" }
-      ]
-    }
+    ignores: ['node_modules/**', 'jest.config.mjs'],
   },
-  ...compat.extends("airbnb"),
+  {
+    files: ['**/*.js', '**/*.test.js'],
+    rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'no-console': 'off',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        { js: 'always' },
+      ],
+      ...pluginJs.configs.recommended.rules,
+    },
+  },
+  ...compat.extends('airbnb'),
 ];
