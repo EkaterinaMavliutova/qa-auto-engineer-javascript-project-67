f98dfc4e5af7e298bcbae60e4a1ba27859c05ead
ekaterinamavliutova <akumaaya@gmail.com>
2024-06-12 18:11:07 +0200

    set up testing with jest

---
+++ __tests__/pageLoader.test.js
@@ -0,0 +1,24 @@
+import nock from 'nock';
+import fsp from 'node:fs/promises';
+import path from 'node:path';
+import os from 'node:os';
+import { pageLoader } from '../src/pageLoader.js';
+import { readTestFile } from './utils.js';
+
+nock.disableNetConnect();
+
+let tempDir = '';
+
+beforeEach(async () => {
+  tempDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
+test('pageLoader returns expected file path', async () => {
+  const link = new URL('https://ru.hexlet.io/courses');
+  nock(link.host)
+    .get(link.pathname)
+    .reply(200, readTestFile('expected.html'));
+  const result = await pageLoader(link, tempDir);
+  expect(result).toEqual({
+    filepath: path.join(tempDir, 'ru-hexlet-io-courses.html'),
+  });
+});
---
+++ __tests__/utils.js
@@ -0,0 +1,10 @@
+// import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs';
+import { __dirname } from '../eslint.config';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+export const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
+export const readTestFile = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8');
